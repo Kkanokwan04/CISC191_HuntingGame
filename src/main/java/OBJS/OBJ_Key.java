462205d8787977816d89627d5c337d146f1f7eec
kkanokwan04 <kpararod@student.sdccd.edu>
2024-10-05 23:12:59 -0700

    init commit

---
+++ src/main/java/OBJS/OBJ_Key.java
@@ -0,0 +1,24 @@
+package OBJS;
+
+import edu.sdccd.cisc191.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Key extends SuperObject {
+
+    GamePanel gp;
+    public OBJ_Key(GamePanel gp){
+        this.gp = gp;
+
+        name = "Key";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/key.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
